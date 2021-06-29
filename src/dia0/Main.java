15d446bc945b2415409d82c34594c894a2668d51
Bia <a.beeatriz@gmail.com>
2021-06-29 16:10:19 -0300

    Commit inicial.

---
+++ src/dia0/Main.java
@@ -0,0 +1,17 @@
+package dia0;
+
+public class Main {
+
+//    _public - acessível por outros objetos
+//    _static - método da classe e não do objeto
+//    _void - não declarativo /sem retorno
+//    main(String[] args) {
+//
+//    }
+
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!!");
+        System.out.println(System.getProperty("java.version"));
+    }
+
+}
